bf3a17a3c5c3401a6ec4a18b45c084e8bd911285
Jeong Hyunji <guswl3201@naver.com>
2022-05-29 21:02:43 +0900

    crawling 코드  변경

--- crawling/content.py
+++ crawling/content.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import pyautogui
 from openpyxl import Workbook
 from tqdm import tqdm
-from datetime import date,datetime
+from datetime import date, datetime
 import re
 import pandas as pd
 
@@ -12,10 +12,7 @@ import pandas as pd
 
 team = pyautogui.prompt("구단 이름을 입력하세요")
 
-fpath = r'C:\Users\Jeong Hyunji\Desktop\baseball\crawling\크롤링.xlsx'
-
-page = 0 
-
+fpath = r'C:\Users\Jeong Hyunji\Desktop\baseball\crawling\크롤링_1.xlsx'
 
 wb = openpyxl.load_workbook(fpath)
 ws = wb[team]
@@ -27,15 +24,27 @@ for index,url in tqdm(enumerate(urls)):
 
     i = index + 2
 
-    response = requests.get(url)
-    html = response.text
-    soup = BeautifulSoup(html,'html.parser')
-    contents = soup.find(id= "contentDetail")
-    content = str(contents)
-    content = re.sub('(<([^>]+)>)', ' ', content).strip()
-    content = re.sub(' +', ' ', content)
-    ws.cell(row=i, column=3).value = content
-    
-    print(i, content)
+    if (ws.cell(row= i, column=3).value == " "):
+        try:
+            response = requests.get(url)
+            html = response.text
+            soup = BeautifulSoup(html,'html.parser')
+            contents = soup.find(id= "contentDetail")
+            content = str(contents)
+            content = re.sub('(<([^>]+)>)', ' ', content).strip()
+            content = re.sub(' +', ' ', content)
+            content = re.sub('[^A-Za-z0-9가-힣]', ' ', content)
+            ws.cell(row=i, column=3).value = content
+            print(i, content)
+
+        except requests.exceptions.Timeout as errd:
+            print("Timeout Error : ", errd)
+        except requests.exceptions.HTTPError as errb:
+            print("Http Error : ", errb)
+        except requests.exceptions.RequestException as erra:
+            print("AnyException : ", erra)
+
+    if (i%100==0):
+        wb.save(fpath)
 
 wb.save(fpath)
