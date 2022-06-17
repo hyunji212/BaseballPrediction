13a67a4625f74fbae426debcdacad779c8d89110
Jeong Hyunji <guswl3201@naver.com>
2022-06-17 15:21:30 +0900

    homechange

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -19,82 +19,12 @@ function Home() {
     </div>
     </div>
 
-                <div className="intro2" style={{overflowX: 'hidden'}}>
-                        <div className="title1">
-                            KBO가 제공하는 서비스
-                        </div>
-                        <ul className="service1">
-                            <li>
-                            <div className="icon1">
-                                <img src="" width={40} height={40} alt=""/>
-                            </div>
-                            <div className="subtitle1">
-                                팀별 순위 예측
-                            </div>
-                            <div className="content1">
-                                팀별 순위 예측에 관한 설명
-                            </div>
-                            </li>
-                            <li>
-                            <div className="icon2">
-                                <img src="" width={40} height={40} alt=""/>
-                            </div>
-                            <div className="subtitle2">
-                                선수별 랭킹
-                            </div>
-                            <div className="content2">
-                                선수별 랭킹에 관한 설명
-                            </div>
-                            </li>
-                            <li>
-                            <div className="icon3">
-                                <img src="" width={40} height={40} alt=""/>
-                            </div>
-                            <div className="subtitle3">
-                                선수별 순위 예측
-                            </div>
-                            <div className="content3">
-                                선수별 순위 예측에 관한 설명
-                            </div>
-                            </li>
-                        </ul>
-                        <ul className="service2">
-                            <li>
-                            <div className="icon4">
-                                <img src="" width={40} height={40} alt=""/>
-                            </div>
-                            <div className="subtitle4">
-                                검색
-                            </div>
-                            <div className="content4">
-                                검색에 관한 설명
-                            </div>
-                            </li>
-                            <li>
-                            <div className="icon5">
-                                <img src="" width={40} height={40} alt=""/>
-                            </div>
-                            <div className="subtitle5">
-                                우리팀 응원하기
-                            </div>
-                            <div className="content5">
-                                우리팀 응원하기에 관한 설명
-                            </div>
-                            </li>
-                            <li>
-                            <div className="icon6">
-                                <img src="" width={40} height={40} alt=""/>
-                            </div>
-                            <div className="subtitle6">
-                                최근 경기 기록
-                            </div>
-                            <div className="content6">
-                                최근 경기 기록에 관한 설명
-                            </div>
-                            </li>
-                        </ul>
-                </div>
-                </div>
+    <div className="intro2" style={{overflowX: 'hidden'}}>
+        <div className="title1"> KBO가 제공하는 서비스 </div>
+                       
+                       
+        </div>
+    </div>
   );
 }
 
--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -3,16 +3,12 @@ import {NavLink, useLocation} from "react-router-dom";
 import "../css/SideBar.css";
 
 function SideBar() {
-  const Team_ARR = [
-  {id: 0, name:"KIA"},{id:1, name:"롯데"},{id:2, name:"삼성"},
-  {id:3, name:"두산"},{id:4, name:"LG"},{id:5, name:"SSG"},{id:6, name:"한화"},
-  {id:7, name:"키움"},{id:8, name:"NC"},{id:9, name:"kt"}]
-  const pathName = useLocation().pathname;
+  const Team_Arr = ["KIA","롯데","삼성","두산","LG","SSG","한화","키움","NC","kt" ]
 
   return (
     <div className="sidebar">
         {Team_ARR.map((team,idx)=> (
-              <NavLink to={`/Team/${team.id}`} key={idx} style={{ textDecoration: 'none'}}>
+              <NavLink to={`/Team/${idx}`} key={idx} style={{ textDecoration: 'none'}}>
                 {team.name}
               </NavLink>
         ))}
