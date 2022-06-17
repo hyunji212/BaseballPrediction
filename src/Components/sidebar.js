302f694db1084c6610e7ef95369a72b4d0091d82
Jeong Hyunji <guswl3201@naver.com>
2022-06-17 14:22:09 +0900

    c

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,20 +1,19 @@
 import React from "react";
 import {NavLink, useLocation} from "react-router-dom";
 import "../css/SideBar.css";
-import SideBarItem from "./SideBarItem";
 
 function SideBar() {
-  const Team_ARR = [{name:"KIA"},{name:"롯데"},{name:"삼성"},{name:"두산"},{name:"LG"},{name:"SSG"},{name:"한화"},{name:"키움"},{name:"NC"},{name:"kt"}]
+  const Team_ARR = [
+  {id: 0, name:"KIA"},{id:1, name:"롯데"},{id:2, name:"삼성"},
+  {id:3, name:"두산"},{id:4, name:"LG"},{id:5, name:"SSG"},{id:6, name:"한화"},
+  {id:7, name:"키움"},{id:8, name:"NC"},{id:9, name:"kt"}]
   const pathName = useLocation().pathname;
 
   return (
     <div className="sidebar">
         {Team_ARR.map((team,idx)=> (
-              <NavLink to={`/Team/${idx}`} key={idx} style={{ textDecoration: 'none'}}>
-                <SideBarItem
-                    team = {team}
-                    isActive={pathName===`/Team/${idx}` ? true : false} 
-                />
+              <NavLink to={`/Team/${team.id}`} key={idx} style={{ textDecoration: 'none'}}>
+                {team.name}
               </NavLink>
         ))}
     </div>
