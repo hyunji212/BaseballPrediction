3d1d2dd1d480b83403e458b4b502979c63ce3c5e
Jeong Hyunji <guswl3201@naver.com>
2022-06-17 13:27:35 +0900

    c

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "../css/Home.css";
 import TotalNav from "./TotalNav";
+import NowRanking from "./NowRanking"
+import {NavLink} from "react-router-dom";
+import { Button} from 'react-bootstrap';
+
 
 function Home() {
   return (
     <div style={{overflowX: 'hidden'}}>
-    <div className="intro" style={{overflowX: 'hidden'}}>
-    <TotalNav/>                   
-    
-                    <div className="intro_text">
-                        <h1>현재 <br/> 구단 순위</h1>
-                        <h4>2022 KBO 정규리그 10개 팀의 승률입니다. <br /> 각 팀을 누르면 팀별 페이지로 이동합니다.</h4>
-                        <button className="trial" type="button" onclick="alert('Hello World!')">Start free trial</button>
-                    </div>
-                </div>
+     <div className="intro" style={{overflowX: 'hidden'}}>
+      <TotalNav/>                   
+       <div className="intro_text">
+       <h1>현재 <br/> 구단 순위</h1>
+       <h4>2022 KBO 정규리그 10개 팀의 승률입니다. <br /> 각 팀을 누르면 팀별 페이지로 이동합니다.</h4>
+     <NavLink to="/MainTeam"> <Button className="trial"> Start free trial </Button></NavLink>
+     <NowRanking/>
+    </div>
+    </div>
+
                 <div className="intro2" style={{overflowX: 'hidden'}}>
                         <div className="title1">
                             KBO가 제공하는 서비스
---
+++ src/Components/NowRanking.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NowRanking() {
+  return (
+    <div>
+ 
+
+    </div>
+  );
+}
+
+export default NowRanking;
