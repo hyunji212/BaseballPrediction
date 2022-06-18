6c208df9865f7f4cc341356f3c97b4e891044318
Jeong Hyunji <guswl3201@naver.com>
2022-06-18 22:07:17 +0900

    home team버튼 추가

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,6 +8,8 @@ import ServiceList from "./ServiceList"
 
 
 function Home() {
+  const Team_Arr = ["KIA","롯데","삼성","두산","LG","SSG","한화","키움","NC","kt" ];
+
   return (
     <div style={{overflowX: 'hidden'}}>
      <div className="intro" style={{overflowX: 'hidden'}}>
@@ -16,17 +18,17 @@ function Home() {
        <h1>현재 <br/> 구단 순위</h1>
        <h4>2022 KBO 정규리그 10개 팀의 승률입니다. <br /> 각 팀을 누르면 팀별 페이지로 이동합니다.</h4>
      <NavLink to="/MainTeam"> <Button className="trial"> Start free trial </Button></NavLink>
-     <NowRanking/>
+     <br></br>
+     {Team_Arr.map((team,idx)=> (
+              <NavLink to={`/Team/${idx}`} key={idx} style={{ textDecoration: 'none'}}>
+               <Button className="teamselect"> {team}</Button> 
+              </NavLink>
+        ))}
+      <NowRanking/>
     </div>
     </div>
 
     <div className="intro2" style={{overflowX: 'hidden'}}>
-<<<<<<< HEAD
-        <div className="title1"> KBO가 제공하는 서비스 </div>
-                       
-                       
-        </div>
-=======
       <div className="title1">
         KBO가 제공하는 서비스
       </div>
@@ -39,7 +41,6 @@ function Home() {
          <ServiceList id="6" title="6제목" content="6내용"/>                    
       </div>
     </div>
->>>>>>> 86ec33abe5a4951d4cdc1f51eb499d6bbf8dd15c
     </div>
   );
 }
--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -3,13 +3,13 @@ import {NavLink, useLocation} from "react-router-dom";
 import "../css/SideBar.css";
 
 function SideBar() {
-  const Team_Arr = ["KIA","롯데","삼성","두산","LG","SSG","한화","키움","NC","kt" ]
+  const Team_Arr = ["KIA","롯데","삼성","두산","LG","SSG","한화","키움","NC","kt" ];
 
   return (
     <div className="sidebar">
-        {Team_ARR.map((team,idx)=> (
+        {Team_Arr.map((team,idx)=> (
               <NavLink to={`/Team/${idx}`} key={idx} style={{ textDecoration: 'none'}}>
-                {team.name}
+                {team}
               </NavLink>
         ))}
     </div>
