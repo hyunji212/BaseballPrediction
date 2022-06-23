97038159dad73b2d7eda39d775d3a5bae3dff779
JungKyungSong <lordsong5974@gmail.com>
2022-06-23 20:33:46 +0900

    c

--- src/Components/ServiceList.js
+++ src/Components/ServiceList.js
@@ -34,11 +34,13 @@ function ServiceList(props) {
   
 
   return (
+    <div className='service_container'>
       <div className='Service_List'>
         <span>
           {content()}
         </span>
       </div>
+    </div>
   )
 }
 
