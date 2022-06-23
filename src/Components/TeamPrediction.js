db7b0cbd82e3f5314cd1085e6a7424c1daedcdf1
JungKyungSong <lordsong5974@gmail.com>
2022-06-23 20:02:27 +0900

    cssmainteam

--- src/Components/TeamPrediction.js
+++ src/Components/TeamPrediction.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "../css/MainTeam.css";
 import NowRanking from "./NowRanking"
+import SideBar from "./sidebar";
 
 function TeamPredicion() {
   return (
     <div className="mainteam">
+      <div className="mainteam_sidebar">
+        <SideBar/>
+      </div>
       <div className="mainteam_title">
          KBO 정규리그 현재 순위&예측 순위
       </div>
+      <div className="mainteam_ranking_title" style={{fontWeight: "bold", color:"white", fontSize: "20px", marginLeft: "71vw", marginTop: "2vh"}}>현재 순위</div>
       <div className="mainteam_ranking">
-        <p className="mainteam_ranking_title" style={{fontWeight: "bold", color:"white", fontSize: "20px"}}>현재 순위</p>
         <NowRanking/>
-        </div>
+      </div>
     </div>
   );
 }
